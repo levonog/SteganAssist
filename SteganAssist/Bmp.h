f3336119e7b021d1d83031a5015c637b66e706df
Levon Oganesyan <levonog@yandex.ru>
2018-06-11 22:06:50 +0400

    Bmp some types added

--- SteganAssist/Bmp.h
+++ SteganAssist/Bmp.h
@@ -3,10 +3,27 @@
 #include"ImageFileBuffer.h"
 #include"Image.h"
 
+// [ICC.1:2010]
 class Bmp : public Image
 {
 	InputBitStream _image_content;
+	enum class types
+	{
+		dateTimeNumber,
+		float32Number,
+		positionNumber,
+		response16Number,
+		s15Fixed16Number,
+		u16Fixed16Number,
+		u1Fixed15Number,
+		u8Fixed8Number,
+		uInt8Number,
+		uInt16Number,
+		uInt32Number,
+		uInt64Number,
+		XYZNumber,
 
+	};
 
 
 
